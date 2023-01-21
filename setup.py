f994e45c5a7b24a820d68d9eae6f27e7eb4834c4
cosmoquester <cosmoquester@gmail.com>
2023-01-21 11:56:58 +0900

    build: Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setup(
     version="1.0.0",
     description="인공지능 북커톤 대회 모델 학습을 위한 레포지토리",
     python_requires=">=3.7",
-    install_requires=[],
+    install_requires=[
+        "torch",
+        "pytorch-lightning",
+        "transformers",
+        "wandb",
+        "deepspeed",
+    ],
     url="https://github.com/khu-bot/ai-essayist.git",
     packages=find_packages(exclude=["tests"]),
 )

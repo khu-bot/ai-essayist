30c96ec2454b6cbb586ba2dcf18ec65eb505adeb
cosmoquester <cosmoquester@gmail.com>
2023-01-21 11:27:43 +0900

    build: Set version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ai-essayist",
-    version="0.0.1",
+    version="1.0.0",
     description="인공지능 북커톤 대회 모델 학습을 위한 레포지토리",
     python_requires=">=3.7",
     install_requires=[],
